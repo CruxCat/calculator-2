a30e94fc9651af0787b2f1388438a9a03595ef18
Catherine Vu <89547780+CruxCat@users.noreply.github.com>
2022-05-03 19:15:06 -0700

    Welcome Statement

--- calculator.py
+++ calculator.py
@@ -4,4 +4,16 @@ from arithmetic import (add, subtract, multiply, divide, square, cube,
                         power, mod, )
 
 
+#welcome statement
+#while true loop
+#enter math function, two numbers
+#input statement >>
+#tokenize input using split by space
+#if statement and quit if input == "q" "BREAK" loop
+#else solve the equation
+#return answer 
+
 # Replace this with your code
+
+print("Welcome to the Calculator. Please enter your equation and 2 numbers. Enjoy.")
+while True:
